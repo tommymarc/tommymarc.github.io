13cb785b65af31b9ea94c0e2d82b1a7d26fce6c0
tommymarc <tommymarc@sina.com>
2020-05-22 19:58:17 +0800

    oreooo

---
+++ oreooo/precache-manifest.2a57e13b58ecacad98f95da7aef1c956.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a9947b758926edf27a9",
+    "url": "./css/app.237a236f.css"
+  },
+  {
+    "revision": "0a9947b758926edf27a9",
+    "url": "./js/app.7a510c7d.js"
+  },
+  {
+    "revision": "05e3c2db7921aa1f38b6",
+    "url": "./js/chunk-vendors.f92f98fa.js"
+  },
+  {
+    "revision": "f55d75eb5275de80d836d268cb71ac29",
+    "url": "./img/oreo.f55d75eb.png"
+  },
+  {
+    "revision": "86adc167d797d4659fe119634d127338",
+    "url": "./img/O.86adc167.png"
+  },
+  {
+    "revision": "8274afb073f69f59829439ab4b9b3693",
+    "url": "./img/tip.8274afb0.svg"
+  },
+  {
+    "revision": "e908f02612700c8c4e7b552f0a7c8856",
+    "url": "./fonts/Seto.e908f026.eot"
+  },
+  {
+    "revision": "401d3807883669d0101cab4e5e361849",
+    "url": "./fonts/Seto.401d3807.woff"
+  },
+  {
+    "revision": "98f7e6640de0050107f56b8509b8e551",
+    "url": "./fonts/Seto.98f7e664.ttf"
+  },
+  {
+    "revision": "43f8ed00440e70f0a33f5fdb2b4ec7ad",
+    "url": "./img/Seto.43f8ed00.svg"
+  },
+  {
+    "revision": "0ecc63ff81b3cef910615fb1ace7cc11",
+    "url": "./img/random.0ecc63ff.svg"
+  },
+  {
+    "revision": "e2a76ff4b753fde918acd6bdc8f29b6a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "./robots.txt"
+  }
+];
